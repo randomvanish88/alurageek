67b3bb3aac9effc625799170a2b662da393080d6
agustin gauna <randomvanish88@gmail.com>
2024-05-26 18:59:45 -0300

    actualizacion

--- assets/pages/login/index.js
+++ assets/pages/login/index.js
@@ -1,64 +1,53 @@
-import { footer, nav } from '../../js/navHeader/index.js'
-
+import { footer, nav } from '../../js/navHeader/index.js';
 
 window.onload = function () {
   const paths = {
-  logotipo: "../../img/alurageek.svg",
-  lupa: "../../img/lupa.png",
-  home: "../../../index.html",
-  url: "../product/index.html",
-  login: "",
-  inputShow: false
-  } ;
+    logotipo: "../../img/alurageek.svg",
+    lupa: "../../img/lupa.png",
+    home: "../../../index.html",
+    url: "../product/index.html",
+    login: "",
+    inputShow: false
+  };
 
   nav(paths);
-  footer(paths); 
+  footer(paths);
+
   
-  
-  const loginForm = document.getElementById('loginForm');
-  loginForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+  function validar() {
+    console.log("La función validar está funcionando.");
+
     
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+
+    
     const errorMessages = document.getElementById('errorMessages');
+
     
-    // Clear previous error messages
-    errorMessages.innerHTML = '';
-    
-    // Frontend validation
-    if (!validateEmail(email)) {
-      errorMessages.innerHTML += '<p>Email inválido.</p>';
+    let errorMessage = '';
+
+   
+    if (email === '') {
+      errorMessage += 'El campo de correo electrónico está vacío.<br>';
     }
 
-    if (password.length < 6) {
-      errorMessages.innerHTML += '<p>La contraseña debe tener al menos 6 caracteres.</p>';
+    if (password === '') {
+      errorMessage += 'El campo de contraseña está vacío.<br>';
     }
 
-    if (errorMessages.innerHTML === '') {
-      // No errors, proceed with form submission
-      try {
-        const response = await fetch('http://localhost:5000/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ email, password })
-        });
-
-        if (response.ok) {
-          alert('Login successful');
-        } else {
-          alert('Invalid email or password');
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
+    if (errorMessage !== '') {
+      errorMessages.innerHTML = errorMessage;
+    } else {
+      errorMessages.innerHTML = '';
+      // Simular redireccionamiento después de un inicio de sesión exitoso
+      alert('Inicio de sesión exitoso.');
+      window.location.href = "../../../index.html"; // Redirige a la página principal
     }
-  });
-
-  function validateEmail(email) {
-    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return re.test(String(email).toLowerCase());
   }
+
+  // Hacer la función validar disponible globalmente
+  window.validar = validar;
 };
+
+
